eb5ec41fa2c2223d1aa17e5c6032a415e015ba0b
Varun Patro <varun.kumar.patro@gmail.com>
2015-05-08 04:14:10 +0800

    fully complete weather.js and ignore nea credentials from git

--- weather.js
+++ weather.js
@@ -1,2 +1,35 @@
-var parseString = require('xml2js').parseString;
-var restler = require('restler');
+var rest = require('restler');
+var jf = require('jsonfile');
+
+var nea_credentials_path = './nea_credentials.json';
+var neaAuthKey = jf.readFileSync(nea_credentials_path).key;
+
+var nowcastURL = 'http://www.nea.gov.sg/api/WebAPI?dataset=nowcast&keyref=' + neaAuthKey;
+var nowcastJSON;
+var CLEMENTI_nowcast = undefined;
+
+var psiURL = 'http://www.nea.gov.sg/api/WebAPI/?dataset=psi_update&keyref=' + neaAuthKey;
+var psiJSON;
+var westPSI_3HR = undefined;
+var westPSI_24HR = undefined;
+
+rest.get(nowcastURL).on('complete', function (data) {
+    nowCastJSON = data;
+    CLEMENTI_nowcast = data.channel.item[0].weatherForecast[0].area[9].$.forecast; // area[9] is Clementi
+});
+
+rest.get(psiURL).on('complete', function (data) {
+    psiJSON = data;
+    
+    // region[4] is for West Singapore
+    westPSI_24HR = psiJSON.channel.item[0].region[4].record[0].reading[0].$.value;
+    westPSI_3HR = psiJSON.channel.item[0].region[4].record[0].reading[1].$.value;
+});
+
+function getWeather() {
+    return "Clementi weather: " + CLEMENTI_nowcast + '\n 24 Hour PSI: ' + westPSI_24HR + ', 3 Hour PSI: ' + westPSI_3HR + '.';
+}
+
+module.exports = {
+    getWeather: getWeather
+}
