0c7ef8c8d6472ca3de4d0dbd418cd487d53d56dc
Varun Patro <varun.kumar.patro@gmail.com>
2015-05-08 17:22:36 +0800

    format bus data api (async style) + create util methods

--- traffic.js
+++ traffic.js
@@ -1,8 +1,11 @@
 var rest = require('restler');
 var jf = require('jsonfile');
+var util = require('./util');
 
 var ltaCredFile = './private/lta_credentials.json';
 var ltaCredentials = jf.readFileSync(ltaCredFile);
+
+var busStops = [19059, 19051, 17099, 17091];
 
 var busStopUrl = 'http://datamall2.mytransport.sg/ltaodataservice/BusArrival?BusStopID=' ;
 
@@ -11,19 +14,27 @@ var busStopHeaders = {
     "UniqueUserID": ltaCredentials.UniqueUserID
 };
 
-function busStop(id) {
+function busStop(id, callback) {
     var req_url = busStopUrl + id.toString();
     var req_options = {"headers": busStopHeaders};
-    var response = send(req_url, req_options);
+    var response = send(req_url, req_options, callback);
     return response;
 }
 
-function send(req_url, req_options) {
-    return rest.get(req_url, req_options).on('complete', process_info);
+function process_info(data, callback) {
+    var processed_data = "Buses in operations:";
+    data.Services.forEach(function(bus) {
+    if (bus.Status === 'In Operation') {
+        processed_data += '\n' + bus.ServiceNo + " \t- " + util.timeLeftMin(new Date(bus.NextBus.EstimatedArrival)) + ", " + util.timeLeftMin(new Date(bus.SubsequentBus.EstimatedArrival));
+            }
+    });
+    callback(processed_data);
 }
 
-function process_info(data) {
-    console.log (data);
+function send(req_url, req_options, callback) {
+    return rest.get(req_url, req_options).on('complete', function(data) {
+        process_info(data, callback);
+    });
 }
 
 module.exports = {
--- util.js
+++ util.js
@@ -2,6 +2,11 @@ function extractPhoneNumber(phoneString) {
     return phoneString.split('@')[0];
 }
 
+function timeLeftMin(time) {
+    return Math.ceil((time - new Date()) / 1000 / 60) + " min";
+}
+
 module.exports = {
-    getPhoneNum: extractPhoneNumber
+    getPhoneNum: extractPhoneNumber,
+    timeLeftMin: timeLeftMin
 }
