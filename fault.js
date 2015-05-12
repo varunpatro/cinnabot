38b4f585ae8dc0a7c911467ac50df54896225029
Varun Patro <varun.kumar.patro@gmail.com>
2015-05-13 00:55:17 +0800

    fault.js finally works

--- fault.js
+++ fault.js
@@ -1,3 +1,5 @@
+var rest = require('restler');
+
 //var postURL = "https://docs.google.com/forms/d/1mh5jD1RfstgrbJPefjyPoM2OyLqsZt6C87g1suQ1TuI/formResponse";
 var postURL = "http://localhost"; // Use this in dev mode
 
@@ -31,17 +33,30 @@ function problemUrgency(num) {
     }
 }
 
-function prepareForm(data, callback) {
+function send(URL, Obj, callback) {
+    callback = (typeof callback === 'function') ? callback : console.log;
+    rest.postJson(URL, Obj).on('complete', function(result, response) {
+        var message;
+        if (!(result instanceof Error)) {
+            message = "Fault reported.";
+        } else {
+            message = "Unable to report. Try again later.";
+        }
+        callback(message);
+    });
+}
+
+function prepareForm(data, user, callback) {
     var postObj = {
         'entry.2132193706': faultCategory(data.faultCategoryOption),
         'entry.2132193706.other_option_response': data.faultCategoryOther,
         'entry.749088216': problemUrgency(data.problemUrgencyOption),
         'entry.2133800720': data.problemLocation,
-        'entry.787536878': name,
-        'entry.1813223989': roomNo,
-        'entry.1836226199': matricNo,
-        'entry.2119962668': nusEmail,
-        'entry.858293967': phone,
+        'entry.787536878': user.name,
+        'entry.1813223989': user.roomNo,
+        'entry.1836226199': user.matricNo,
+        'entry.2119962668': user.nusEmail,
+        'entry.858293967': user.phone,
         'entry.113024073': data.problemDescription,
         'draftResponse': '[,,"6866458541185337193 "] ',
         'pageHistory': "0",
@@ -51,29 +66,17 @@ function prepareForm(data, callback) {
     send(postURL, postObj, callback);
 }
 
-function send(URL, Obj, callback) {
-    callback = (typeof callback === 'function') ? callback : console.log;
-    rest.postJson(URL, Obj).on('complete', function(data, response) {
-        var message;
-        if (response.statusCode < 400) {
-            message = "Fault reported.";
-        } else {
-            message = "Unable to report. Try again later.";
-        }
-        callback(message);
-    });
-}
 
 function report(data, callback) {
-    var isValid = data.problemDescription && data.problemLocation && problemUrgencyOption && faultCategoryOption;
+    var isValid = data.problemDescription && data.problemLocation && data.problemUrgencyOption && data.faultCategoryOption;
     if (isValid) {
         data.faultCategoryOther = data.faultCategoryOther ? data.faultCategoryOther : "";
-        prepareForm(data, callback);
+        prepareForm(data, {}, callback);
     } else {
         return "invalid data";
     }
 }
 
 module.exports = {
-    reportFault: report
+    report: report
 };
