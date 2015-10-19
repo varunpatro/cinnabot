f93fdbfa38df54c90ca8aca9fd13a073dc0c4643
Varun Patro <varun.kumar.patro@gmail.com>
2015-10-19 23:51:25 +0800

    Update weather methods.

--- weather.js
+++ weather.js
@@ -11,9 +11,25 @@ var clementiNowcast;
 
 var psiURL = 'http://www.nea.gov.sg/api/WebAPI/?dataset=psi_update&keyref=' +
     neaAuthKey;
+var pm25URL = 'http://www.nea.gov.sg/api/WebAPI/?dataset=pm2.5_update&keyref=' +
+    neaAuthKey;
 var psiJSON;
+var pmJSON;
 var westPSI3;
 var westPSI24;
+var westPM25;
+var timeStamp;
+
+function getTime(timeStr) {
+    var yy = timeStr.substr(0, 4);
+    var MM = timeStr.substr(4, 2);
+    var dd = timeStr.substr(6, 2);
+    var HH = timeStr.substr(8, 2);
+    var mm = timeStr.substr(10, 2);
+    var ss = timeStr.substr(12, 2);
+    var str = MM + ' ' + dd + ' ' + yy + ' ' + HH + ':' + mm + ':' + ss;
+    return new Date(str);
+}
 
 rest.get(nowcastURL).on('complete', function(data) {
     nowCastJSON = data.channel.item[0];
@@ -25,12 +41,20 @@ rest.get(psiURL).on('complete', function(data) {
 
     // region[4] is for West Singapore
     westPSI24 = psiJSON.region[4].record[0].reading[0].$.value;
-    westPSI3westPSI3 = psiJSON.region[4].record[0].reading[1].$.value;
+    westPSI3 = psiJSON.region[4].record[0].reading[1].$.value;
+});
+
+rest.get(pm25URL).on('complete', function(data) {
+    pmJSON = data.channel.item[0];
+
+    // region[4] is for West Singapore
+    westPM25 = pmJSON.region[3].record[0].reading[0].$.value;
+    timeStamp = pmJSON.region[3].record[0].$.timestamp;
 });
 
 function getWeather() {
-    return 'Clementi weather: ' + clementiNowcast + '\n24 Hour PSI: ' +
-        westPSI24 + '\n3 Hour PSI: ' + westPSI3 + '.';
+    return 'Time: ' + getTime(timeStamp) + '\nClementi weather: ' + clementiNowcast + '\n24 Hour PSI: ' +
+        westPSI24 + '\n3 Hour PSI: ' + westPSI3 + '\n1 Hour PM 2.5: ' + westPM25 + '.';
 }
 
 module.exports = {
