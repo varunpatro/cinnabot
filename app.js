b9f096a00ec1b638304af3a13b446b65a163f6fa
Varun Patro <varun.kumar.patro@gmail.com>
2015-10-19 23:49:01 +0800

    Use Telegram Bot instead of WhatsApp.

--- app.js
+++ app.js
@@ -1,79 +1,17 @@
-var whatsapi = require('whatsapi');
-var logger = require('./logger');
 var jf = require('jsonfile');
-var api = require('./api');
+var TelegramBot = require('node-telegram-bot-api');
+var chalk = require('chalk')
+var credentialsFilePath = './private/telegram_credentials.json';
+var logger = require('./logger')
 
-var credentialsFilePath = './private/wa_credentials.json';
 var CREDENTIALS = jf.readFileSync(credentialsFilePath);
-
-/** CREATE WHATSAPP OBJECT */
-
-var wa = whatsapi.createAdapter({
-    msisdn: CREDENTIALS.phone,
-    username: 'cinnabot',
-    password: CREDENTIALS.password,
-    ccode: CREDENTIALS.cc
+var bot = new TelegramBot(CREDENTIALS.token, {
+    polling: true
 });
 
-/** START CONNECTION */
-
-wa.connect(function connected(err) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log('Connected');
-    // Now login
-    wa.login(logged);
-});
-
-/** EVENT HANDLERS */
-
-wa.on('receivedMessage', function(messageObj) {
-    var responseObj = api.request(messageObj, sendMsg);
-    var responsePhone = responseObj.phone;
-    var responseMessage = responseObj.message;
-
-    if (responseObj.type === 'function') {
-        // do something for async apis
-    } else if (responseObj.type === 'image') {
-        var responseImgURL = responseObj.message;
-        wa.sendImage(responsePhone, responseImgURL, function(err, id) {
-            if (err) {
-                console.log(responseImgURL);
-                console.log(err.message);
-                return;
-            }
-        });
-    } else {
-        sendMsg(responsePhone, responseMessage);
-    }
-
-    logger.logMessage(messageObj, responseObj);
-    logger.storeLogs();
-
-    console.log(messageObj.body);
-    console.log("From: " + messageObj.from);
-});
-
-/** CALL BACKS */
-
-function logged(err) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log('Logged in to WA server');
-    wa.sendIsOnline();
-}
-
-/** HELPER FUNCTIONS */
-
-function sendMsg(responsePhone, responseMessage) {
-    wa.sendMessage(responsePhone, responseMessage, function(err, id) {
-        if (err) {
-            console.log(err.message);
-            return;
-        }
-    });
-}
+console.log(chalk.blue("============================"))
+console.log(chalk.blue("                            "))
+console.log(chalk.blue("      TeleTham Started      "))
+console.log(chalk.blue("                            "))
+console.log(chalk.blue("============================"))
+console.log(chalk.blue("                            "))
