5feed7c62306bd57594e5624a5305834e48f5ea3
Varun Patro <varun.kumar.patro@gmail.com>
2015-11-05 19:32:16 +0800

    Revert "temporarily remove nea now cast."
    This reverts commit 9306914dee7197fefa94d7182fcd5ab7cd85bb5e.

--- weather.js
+++ weather.js
@@ -10,16 +10,15 @@ var psiURL = 'http://sgp.si/now.json';
 
 function getWeather(callback) {
     rest.get(nowcastURL).on('complete', function(data) {
-        // var nowCastJSON = data.channel.item[0];
-        // var clementiNowcast = nowCastJSON.weatherForecast[0].area[9].$.forecast; // area[9] is Clementi
+        var nowCastJSON = data.channel.item[0];
+        var clementiNowcast = nowCastJSON.weatherForecast[0].area[9].$.forecast; // area[9] is Clementi
         rest.get(psiURL).on('complete', function(data) {
             var time = new Date(data.time);
             var westPSI_24HR = data.west.psi_24h;
             var westPSI_3HR = data.overall.psi_3h;
             var westPM2_5_1HR = data.west.pm2_5_1h;
-            var msg = 'Time: ' + util.formatTime(time) + '\n\n';
-            // msg += 'Clementi weather: ' + clementiNowcast + '\n';
-            msg += '24 Hour PSI: ' + westPSI_24HR + '\n3 Hour PSI: ' + westPSI_3HR + '\n1 Hour PM 2.5: ' + westPM2_5_1HR + '.';
+            var msg = 'Time: ' + util.formatTime(time) + '\n\nClementi weather: ' + clementiNowcast + '\n24 Hour PSI: ' +
+                westPSI_24HR + '\n3 Hour PSI: ' + westPSI_3HR + '\n1 Hour PM 2.5: ' + westPM2_5_1HR + '.';
             callback(msg);
         });
     });
