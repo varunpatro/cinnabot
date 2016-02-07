6cb6d91241e07ac8dc737b6309419bd33fe44703
Varun Patro <varun.kumar.patro@gmail.com>
2016-02-07 15:36:34 +0800

    Add dining table in db.

--- db.js
+++ db.js
@@ -1,20 +1,24 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('cinnabot.db');
 
-var LOGSCHEMA = "CREATE TABLE IF NOT EXISTS log (time INTEGER, msg TEXT, userid INTEGER, username TEXT, firstname TEXT, lastname TEXT, latitude REAL, longitude REAL)";
-var FEEDBACKSCHEMA = "CREATE TABLE IF NOT EXISTS feedback (time INTEGER, msg TEXT, userid INTEGER, username TEXT, firstname TEXT, lastname TEXT)";
-var USERSCHEMA = "CREATE TABLE IF NOT EXISTS user (time INTEGER, userid INTEGER PRIMARY KEY, matric TEXT, name TEXT, email TEXT, gender TEXT, isCinnamonResident INTEGER, token TEXT)";
+var LOGSCHEMA = 'CREATE TABLE IF NOT EXISTS log (time INTEGER, msg TEXT, userid INTEGER, username TEXT, firstname TEXT, lastname TEXT, latitude REAL, longitude REAL)';
+var FEEDBACKSCHEMA = 'CREATE TABLE IF NOT EXISTS feedback (time INTEGER, msg TEXT, userid INTEGER, username TEXT, firstname TEXT, lastname TEXT)';
+var USERSCHEMA = 'CREATE TABLE IF NOT EXISTS user (time INTEGER, userid INTEGER PRIMARY KEY, matric TEXT, name TEXT, email TEXT, gender TEXT, isCinnamonResident INTEGER, token TEXT)';
+var DININGSCHEMA = 'CREATE TABLE IF NOT EXISTS dining (time INTEGER, name TEXT, email TEXT, mealperiod TEXT, stall TEXT, rating INTEGER, feedback TEXT)';
+
 db.run(LOGSCHEMA);
 db.run(FEEDBACKSCHEMA);
 db.run(USERSCHEMA);
+db.run(DININGSCHEMA);
 
-var logStmt = db.prepare("INSERT INTO log VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-var feedbackStmt = db.prepare("INSERT INTO feedback VALUES (?, ?, ?, ?, ?, ?)");
-var userStmt = db.prepare("INSERT OR IGNORE INTO user VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+var logStmt = db.prepare('INSERT INTO log VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
+var feedbackStmt = db.prepare('INSERT INTO feedback VALUES (?, ?, ?, ?, ?, ?)');
+var userStmt = db.prepare('INSERT OR IGNORE INTO user VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
+var diningStmt = db.prepare('INSERT OR IGNORE INTO dining VALUES (?, ?, ?, ?, ?, ?, ?)');
 
 function getAllUsers(callback) {
     var users = [];
-    db.all("SELECT DISTINCT userid FROM log", callback);
+    db.all('SELECT DISTINCT userid FROM log', callback);
 }
 
 function getLogStmt() {
@@ -29,24 +33,29 @@ function getUserStmt() {
     return userStmt;
 }
 
+function getDiningStmt() {
+    return diningStmt;
+}
+
 function getLogTable(callback) {
-    return db.all("SELECT * FROM log", callback);
+    return db.all('SELECT * FROM log', callback);
 }
 
 function getUser(userId, callback) {
-    return db.get("SELECT * FROM user where userid=" + userId, callback);
+    return db.get('SELECT * FROM user where userid=' + userId, callback);
 }
 
 function getFeedbackMsg(time, callback) {
-    return db.get("SELECT msg FROM feedback where time=" + time, callback);
+    return db.get('SELECT msg FROM feedback where time=' + time, callback);
 }
 
 module.exports = {
-    "getAllUsers": getAllUsers,
-    "getLogStmt": getLogStmt,
-    "getFeebackStmt": getFeebackStmt,
-    "getLogTable": getLogTable,
-    "getUserStmt": getUserStmt,
-    "getUser": getUser,
-    "getFeedbackMsg": getFeedbackMsg
+    getAllUsers,
+    getLogStmt,
+    getFeebackStmt,
+    getLogTable,
+    getUserStmt,
+    getDiningStmt,
+    getUser,
+    getFeedbackMsg
 };
