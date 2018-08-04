c43a453ae451aa5d9baeb4ab9d9ee9de1b3d8d85
pengnam <seanngpengnam@gmail.com>
2018-08-04 13:47:00 +0800

    Formatting and changing fatal logging to print logging

--- model/db.go
+++ model/db.go
@@ -1,22 +1,21 @@
 package model
 
 import (
-	"github.com/jinzhu/gorm"
+	"errors"
 	"log"
 	"strings"
-	"errors"
+
+	"github.com/jinzhu/gorm"
 )
 
 //For dependency injection
 type DataGroup interface {
 	Add(value interface{})
 	UserGroup(tags []string) []User
-	CheckTagExists (id int, tag string) bool
-	CheckSubscribed (id int, tag string) bool
-	UpdateTag (id int, tag string, flag string) error
-
+	CheckTagExists(id int, tag string) bool
+	CheckSubscribed(id int, tag string) bool
+	UpdateTag(id int, tag string, flag string) error
 }
-
 
 type Database struct {
 	*gorm.DB
@@ -46,9 +45,8 @@ func (db *Database) Add(value interface{}) {
 	db.Create(value)
 }
 
-
 //CheckTagExists takes in an id and a tag and returns whether the tag exists
-func (db *Database) CheckTagExists (id int, tag string) bool {
+func (db *Database) CheckTagExists(id int, tag string) bool {
 	check := db.Where("user_id = ?", id)
 	var usr User
 	if err2 := check.Where(tag+" = ?", "placebo").First(&usr).Error; err2 != gorm.ErrRecordNotFound {
@@ -56,7 +54,7 @@ func (db *Database) CheckTagExists (id int, tag string) bool {
 			return false
 		}
 
-		log.Fatal(err2)
+		log.Print(err2)
 		return false
 	}
 	log.Print("1: True")
@@ -64,30 +62,28 @@ func (db *Database) CheckTagExists (id int, tag string) bool {
 }
 
 //CheckSubscribed takes in an id and a tag and returns true if user is subscribed, false otherwise
-func (db *Database) CheckSubscribed (id int, tag string) bool{
+func (db *Database) CheckSubscribed(id int, tag string) bool {
 	check := db.Where("user_id = ?", id)
 	var usr User
-	if err := check.Where(tag + " = ?", "true").First(&usr).Error; err != gorm.ErrRecordNotFound {
+	if err := check.Where(tag+" = ?", "true").First(&usr).Error; err != gorm.ErrRecordNotFound {
 		return true
 	}
 	log.Print("2: True")
 	return false
 }
 
-
-
 //Update updates the flag for the tag for an User which is determined by the id
-func (db *Database) UpdateTag (id int, tag string, flag string) error{
+func (db *Database) UpdateTag(id int, tag string, flag string) error {
 	check := db.Where("user_id = ?", id)
 	var usr User
 	bannedFields := []string{"user_id", "created_at", "deleted_at"}
-	for _,field := range bannedFields {
+	for _, field := range bannedFields {
 		if field == tag {
 			return errors.New("banned field")
 		}
 	}
 	log.Print(id)
-	check.First(&usr,id)
+	check.First(&usr, id)
 	log.Print(usr)
 
 	if err := check.Model(&usr).Update(tag, flag).Error; err != nil {
@@ -97,7 +93,3 @@ func (db *Database) UpdateTag (id int, tag string, flag string) error{
 	log.Print("3: True")
 	return nil
 }
-
-
-
-
