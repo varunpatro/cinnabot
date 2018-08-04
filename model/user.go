30f0c358c2b443018e535768cd9ba36fd22d350d
pengnam <seanngpengnam@gmail.com>
2018-08-04 13:48:51 +0800

    UserGroup returns all values if all tag is used

--- model/user.go
+++ model/user.go
@@ -1,17 +1,16 @@
 package model
 
 import (
-	"github.com/jinzhu/gorm"
-
 	"time"
+
+	"github.com/jinzhu/gorm"
 )
 
 // User is an ORM compatible struct that serializes a telegram user's information.
 type User struct {
-	UserID    int `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	//DeletedAt time.Time
+	UserID     int `gorm:"primary_key"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 	FirstName  string
 	LastName   string
 	UserName   string
@@ -28,8 +27,15 @@ func (db *Database) UserGroup(tags []string) []User {
 	if len(tags) == 0 {
 		return nil
 	}
+
 	rows, _ := gorm.Open("sqlite3", "../main/cinnabot.db")
 	defer rows.Close()
+	//Returns EVERYONE if all is used
+	if tags[0] == "all" {
+		var users []User
+		rows.Find(&users)
+		return users
+	}
 
 	for i := 0; i < len(tags); i++ {
 		rows = rows.Where(tags[i]+" = ?", "true")
